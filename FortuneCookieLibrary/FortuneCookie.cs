995c04afb87881165bef8879fa304ca39b069483
cristian <cristian.torras@roche.com>
2015-11-03 12:42:43 +0100

    Adaptation for deferred execution

--- FortuneCookieLibrary/FortuneCookie.cs
+++ FortuneCookieLibrary/FortuneCookie.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FortuneCookieLibrary
@@ -68,43 +69,58 @@ namespace FortuneCookieLibrary
             "A chance meeting opens new doors to success and friendship.",
             "You learn from your mistakes... You will learn a lot today.",
             "If you have something good in your life, don't let it go!",
-            "What ever you're goal is in life, embrace it visualize it, and for it will be yours.",
-            "Your shoes will make you happy today.",
-            "You cannot love life until you live the life you love.",
+            "What ever you're goal is in life, embrace it visualize it, and for it will be yours",
+            "Your shoes will make you happy today",
+            "You cannot love life until you live the life you love",
             "Be on the lookout for coming events; They cast their shadows beforehand.",
-            "Land is always on the mind of a flying bird.",
-            "The man or woman you desire feels the same about you.",
-            "Meeting adversity well is the source of your strength.",
-            "A dream you have will come true.",
-            "Our deeds determine us, as much as we determine our deeds.",
-            "Never give up. You're not a failure if you don't give up.",
-            "You will become great if you believe in yourself.",
-            "There is no greater pleasure than seeing your loved ones prosper.",
-            "You will marry your lover.",
-            "A very attractive person has a message for you.",
-            "You already know the answer to the questions lingering inside your head.",
-            "It is now, and in this world, that we must live.",
-            "You must try, or hate yourself for not trying.",
-            "You can make your own happiness.",
-            "The greatest risk is not taking one.",
-            "The love of your life is stepping into your planet this summer.",
-            "Love can last a lifetime, if you want it to.",
-            "Adversity is the parent of virtue.",
-            "Serious trouble will bypass you.",
-            "A short stranger will soon enter your life with blessings to share.",
-            "Now is the time to try something new.",
-            "Wealth awaits you very soon.",
-            "If you feel you are right, stand firmly by your convictions.",
+            "Land is always on the mind of a flying bird",
+            "The man or woman you desire feels the same about you",
+            "Meeting adversity well is the source of your strength",
+            "A dream you have will come true",
+            "Our deeds determine us, as much as we determine our deeds",
+            "Never give up. You're not a failure if you don't give up",
+            "You will become great if you believe in yourself",
+            "There is no greater pleasure than seeing your loved ones prosper",
+            "You will marry your lover",
+            "A very attractive person has a message for you",
+            "You already know the answer to the questions lingering inside your head",
+            "It is now, and in this world, that we must live",
+            "You must try, or hate yourself for not trying",
+            "You can make your own happiness",
+            "The greatest risk is not taking one",
+            "The love of your life is stepping into your planet this summer",
+            "Love can last a lifetime, if you want it to",
+            "Adversity is the parent of virtue",
+            "Serious trouble will bypass you",
+            "A short stranger will soon enter your life with blessings to share",
+            "Now is the time to try something new",
+            "Wealth awaits you very soon",
+            "If you feel you are right, stand firmly by your convictions",
             "If winter comes, can spring be far behind?",
-            "Keep your eye out for someone special.",
-            "You are very talented in many ways.",
-            "A stranger, is a friend you have not spoken to yet.",
-            "A new voyage will fill your life with untold memories.",
-            "You will travel to many exotic places in your lifetime.",
-            "Your ability for accomplishment will follow with success.",
-            "Nothing astonishes men so much as common sense and plain dealing.",
-            "Its amazing how much good you can do if you dont care who gets the credit."
+            "Keep your eye out for someone special",
+            "You are very talented in many ways",
+            "A stranger, is a friend you have not spoken to yet",
+            "A new voyage will fill your life with untold memories",
+            "You will travel to many exotic places in your lifetime",
+            "Your ability for accomplishment will follow with success",
+            "Nothing astonishes men so much as common sense and plain dealing",
+            "Its amazing how much good you can do if you dont care who gets the credit"
         };
         #endregion
+
+        public IEnumerable<string> TellMeMyManyFortunes()
+        {
+            return this.TellMeMyManyFortunes(10);
+        }
+
+        public IEnumerable<string> TellMeMyManyFortunes(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                // We simulate that reading fortune is a costly exercise, this way the believers think it is more realistic
+                Thread.Sleep(1000);
+                yield return this.fortunes.PickRandom();
+            }
+        }
     }
 }
--- MyFortuneCookie/Program.cs
+++ MyFortuneCookie/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,17 +18,35 @@ namespace MyFortuneCookie
 
             while (name != "exit")
             {
-                Console.WriteLine("Tell me your name:");
+                Log("Tell me your name:");
                 name = Console.ReadLine();
 
                 var fortuneTeller = new FortuneCookie(name);
-                var fortune = fortuneTeller.TellMeMyFortune();
 
-                Console.WriteLine(fortune);
+                Log("Reading fortunes...");
+                var fortunes = fortuneTeller.TellMeMyManyFortunes().Where(s => s.EndsWith(".")).Take(2).ToList();
+
+                Log("Fortunes prepared...");
+
+                Log("Do you have fortunes? " + fortunes.Any());
+
+                Log("How many? " + fortunes.Count());
+
+                foreach (var fortune in fortunes)
+                {
+                    Log(fortune);
+                }
             }
 
             Console.WriteLine("Press a key to exit...");
             Console.ReadLine();
+        }
+
+        private static void Log(string s)
+        {
+            var timeStamp = DateTime.Now;
+
+            Console.WriteLine(timeStamp.ToString("HH:mm:ss.fff - ") + s);
         }
 
         private static void SetUpExceptionHandling()
