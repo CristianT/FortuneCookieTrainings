1d52a2583934c7be2e9f0959ba357dfca118edc2
cristian <cristian.torras@roche.com>
2015-11-03 10:14:43 +0100

    Renaming of projects

--- MyFortuneCookie/Properties/AssemblyInfo.cs
+++ MyFortuneCookie/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EasyExceptionHandling")]
+[assembly: AssemblyTitle("MyFortuneCookie")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("EasyExceptionHandling")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyFortuneCookie")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
